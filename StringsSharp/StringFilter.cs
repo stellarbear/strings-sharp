824f049efeee5a88225c5ae59d6e1a66786cb7c8
stellarbears <stellarbears@gmail.com>
2020-02-02 08:26:45 +0300

    initial commit

---
+++ StringsSharp/StringFilter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace StringsSharp
+{
+    public class StringFilter : IDisposable
+    {
+        Dictionary<string, Regex> _filters;
+        public StringFilter(string configurationFile)
+        {
+            if (File.Exists(configurationFile))
+            {
+                try
+                {
+                    Dictionary<string, string> jsonData = JsonConvert.DeserializeObject<Dictionary<string, string>>(ReadFile(configurationFile));
+                    _filters = jsonData.
+                        ToDictionary(k => k.Key, k => 
+                            new Regex(k.Value, RegexOptions.Compiled | 
+                                               RegexOptions.IgnoreCase | 
+                                               RegexOptions.IgnorePatternWhitespace));
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+            else
+                throw new FileNotFoundException($"File does not exist: {configurationFile}");
+        }
+        public void Dispose()
+        {
+            _filters?.Clear();
+            _filters = null;
+        }
+
+        public IEnumerable<string> Scan(string inputString)
+        {
+            if (_filters != null)
+            {
+                if (_filters.Count > 0)
+                {
+                    foreach (KeyValuePair<string, Regex> filter in _filters)
+                    {
+                        if (filter.Value.IsMatch((inputString)))
+                        {
+                            yield return filter.Key;
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool ScanQuick(string inputString)
+        {
+            if (_filters != null)
+            {
+                if (_filters.Count > 0)
+                {
+                    foreach (KeyValuePair<string, Regex> filter in _filters)
+                    {
+                        if (filter.Value.IsMatch((inputString)))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private string ReadFile(string filename)
+        {
+            string result = null;
+
+            if (File.Exists(filename))
+            {
+                using (StreamReader sr = new StreamReader(filename, Encoding.UTF8))
+                {
+                    result = sr.ReadToEnd();
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ StringsTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using StringsSharp;
+using System;
+using System.Text.RegularExpressions;
+
+namespace StringsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = @"strings";
+            string configurationFile = @"strings";
+
+            try
+            {
+                //  Unicode
+                using (StringsSharp.StringsSharp ss = new StringsSharp.StringsSharp(1200, "[\u0020-\u007E]", 4, 16))
+                {
+                    foreach (MatchCollection matches in ss.Scan(filename))
+                    {
+                        //  Process matches here
+                    }
+                }
+
+                //  ASCII
+                using (StringsSharp.StringsSharp ss = new StringsSharp.StringsSharp(1251, "[\x20-\x7E]"))
+                {
+                    using (StringsSharp.StringFilter sf = new StringFilter(configurationFile))
+                    {
+                        foreach (MatchCollection matches in ss.Scan(filename, 256))
+                        {
+                            foreach (Match match in matches)
+                            {
+                                if (sf.ScanQuick(match.Value))
+                                {
+                                    //  Process string here
+                                }
+                                foreach (string regexpTag in sf.Scan(match.Value))
+                                {
+                                    //  Process string here
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //  Handle errors here
+            }
+        }
+    }
+}
